b1d134b1a29ad2c0def95ecc6acc02ced2bf4f4b
Güneş Yurdakul <gunesyurdakul@Meliss-Mac-mini.local>
2017-08-08 12:06:49 +0300

    First Commit

---
+++ GUNES/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GUNES
+//
+//  Created by Güneş Yurdakul on 07/08/2017.
+//  Copyright © 2017 gunesyurdakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    /*override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }*/
+
+
+}
+
